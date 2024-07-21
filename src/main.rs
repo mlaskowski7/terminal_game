c7670cf602053ea5c8726c1b54bcbae2348be805
mlaskowski7 <mtlaskowski7@gmail.com>
2024-07-21 21:09:44 +0200

    working on player func

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, io, time::Duration};
+use std::{error::Error, io, sync::mpsc, thread, time::Duration};
 
 use crossterm::{
     cursor::{Hide, Show},
@@ -7,6 +7,11 @@ use crossterm::{
     ExecutableCommand,
 };
 use rusty_audio::Audio;
+use terminal_game::{
+    frame::{self, new_frame, Drawable},
+    player::Player,
+    render::render,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -27,12 +32,33 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // open second thread for rendering loop
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render(&mut stdout, &last_frame, &last_frame, true);
+
+        loop {
+            let current_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render(&mut stdout, &last_frame, &current_frame, false);
+        }
+    });
+
+    let mut player = Player::new();
+
     // main game loop
     'gameloop: loop {
+        let mut current_frame = new_frame();
         // handle input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -41,9 +67,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // render
+        player.draw(&mut current_frame);
+        let _ = render_tx.send(current_frame);
+        thread::sleep(Duration::from_millis(10));
     }
 
     // quit
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
